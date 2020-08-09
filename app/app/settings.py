89a0ec69ec8e02d2732e28bb05a7fdf486253402
satishnaga1 <nagasatish008@gmail.com>
2020-08-09 23:18:43 +0530

    add new file

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    'django.contrib.staticfiles',   
 ]
 
 MIDDLEWARE = [
