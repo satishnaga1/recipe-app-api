811447a367411941499019f55448f36c772cefc8
satishnaga1 <nagasatish008@gmail.com>
2020-08-09 18:56:12 +0530

    new function is added

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,3 +2,7 @@ def add(a,b):
     """Add two numbers"""
     print("add is working")
     return a + b
+
+def subtract(x, y):
+    print("subtract two number")
+    return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
 
 class ClasTestCase(TestCase):
     
     def test_add_number(self):
         print("Test add is working")
         self.assertEqual(add(3,8), 11)
+        
+        
+    def test_subtract_number(self):
+        print("subtract given number")
+        self.assertEqual(subtract(5, 11), 6)
